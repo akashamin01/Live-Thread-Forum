91fb194e872860e0fb252ca2d8d762cf722e83dd
Akash Amin <38359324+akashamin01@users.noreply.github.com>
2019-02-17 11:11:22 +0530

    Update blogbox.php

--- blogbox.php
+++ blogbox.php
@@ -62,7 +62,6 @@ include 'index.php'
 				<button type="submit" >like</button>
 			</div>
 		</div><div class="col-md-4">
-
 			<div class="blogbox">
 				<img class="img-responsive" style="width: 100%;height: 50%;"  src="pic1.jpg">
 				<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknoncluding Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popversions of Lorem Ipsum</p>
